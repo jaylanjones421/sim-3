c9534965a585bed5753aa90bfab021968d8604fc
Jaylan Jones <jaylanjones421@gmail.com>
2018-02-17 15:27:17 -0600

    added words of encouragement and edited selection values

--- sim3/src/App.js
+++ sim3/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import router from './router'
+import router from './router';
+import AuthView from './Components/AuthView/AuthView';
+import DashView from './Components/DashView/DashView';
+import ProfileView from './Components/ProfileView/ProfileView';
+import SearchView from './Components/SearchView/SearchView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>something is here</h1>
-        {router}
+        <h1>i believe in u</h1>
+        <DashView/>
+        {/*router*/}
+        {/*wait to implement router until all views are done, 
+        you can test views by changing above component*/}
       </div>
     );
   }
--- sim3/src/Components/DashView/DashView.js
+++ sim3/src/Components/DashView/DashView.js
@@ -5,16 +5,16 @@ import FriendCard from "../FriendCard/FriendCard";
 
 class DashView extends Component {
   render() {
-      let reccomendedFriends = this.props.reccomendedFriendsList.map((friend,i)=>{
+     /* let reccomendedFriends = this.props.reccomendedFriendsList.map((friend,i)=>{
           return <FriendCard 
           userImg={this.props.userImg}
           userFirstName={this.props.userFirstName}
           userLastName={this.props.userLastName}
           />
-      })
+      })*/
     return (
       <div>
-        <Navbar page="Dashboard" />
+        <Navbar page="Dashbaord" />
         <div className="dashTop">
           <EditUserCard />
           <div className="aboutText">
@@ -32,13 +32,13 @@ class DashView extends Component {
                     <div>Sorted By </div>
                     <div>
                         <select name="sortFriends" id="">
-                            <option value="firstName"></option>
-                            <option value="lastName"></option>
-                            <option value="gender"></option>
-                            <option value="hobby"></option>
-                            <option value="hairColor"></option>
-                            <option value="eyeColor"></option>
-                            <option value="birthday"></option>
+                            <option value="firstName">First Name</option>
+                            <option value="lastName">Last Name</option>
+                            <option value="gender">Gender</option>
+                            <option value="hobby">Hobby</option>
+                            <option value="hairColor">Hair Color</option>
+                            <option value="eyeColor">Eye Color</option>
+                            <option value="birthday">Birthday</option>
                         </select>
                     </div>
                 </div>
